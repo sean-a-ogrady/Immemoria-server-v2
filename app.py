c8ad90ee61094493ebabcee5f94a967babefa4e1
Sean O'Grady <ogrady.sean.a@gmail.com>
2023-12-20 08:33:13 -0500

    Add user authentication and authorization routes

--- app.py
+++ app.py
@@ -2,6 +2,9 @@ from flask import Flask, make_response, session, jsonify, request
 from flask_migrate import Migrate
 from flask_cors import CORS
 from models.model_config import db
+from auth.authorization_required import authorization_required
+from routes.user_routes import UserRoutes
+from models.user import User
 
 # Initialize Flask app and SQLAlchemy database
 app = Flask(__name__)
@@ -12,26 +15,81 @@ migrate = Migrate(app, db)
 CORS(app, supports_credentials=True)
 app.secret_key = "secret" # TODO: Change this to something more secure, like a randomly generated UUID
 
+only = () # TODO: Add fields to this tuple to limit the fields returned by the API
+
 ########################################################################
 ########################### API entry point ############################
 ########################################################################
 
-# Auth test
-@app.route("/api/auth_test", methods=["GET"])
-def auth_test():
-    """Test authentication."""
-    pass
+@app.route("/")
+@authorization_required
+def index():
+    return make_response({"msg": "API access granted.", "user": authorization.get_json()}, 200)
 
+########################################################################
+########################## Authorization ###############################
+########################################################################
+
+@app.route("/signup", methods=["POST"])
+def add_user():
+    if request.method == "POST":
+        payload = request.get_json()
+
+        email_address = payload.get("email_address")
+        username = payload.get("username")
+        password = payload.get("password")
+
+        # Call the add_user method from UserRoutes
+        response = UserRoutes.add_user(
+            email_address,
+            username,
+            password
+        )
+
+        return response
+    else:
+        return make_response({"error": f"Invalid request type. (Expected POST; received {request.method}.)"}, 400)
+
+@app.route("/login", methods=["POST"])
+def login_user():
+    if request.method == "POST":
+        payload = request.get_json()
+
+        matching_user = user.query.filter(user.email_address.like(f"%{payload['email_address']}%")).first()
+
+        AUTHENTICATION_IS_SUCCESSFUL = bcrypt.checkpw(
+            password=payload["password"].encode("utf-8"),
+            hashed_password=matching_user.password.encode("utf-8")
+        )
+
+        if matching_user is not None and AUTHENTICATION_IS_SUCCESSFUL:
+            session["user_id"] = matching_user.id
+            return make_response(
+                {
+                    "message": "Login successful",
+                    "user": matching_user.to_dict(only=only)
+                }
+            , 200)
+        else:
+            return make_response({"error": "Invalid credentials."}, 401)
+    else:
+        return make_response({"error": f"Invalid request type. (Expected POST; received {request.method}.)"}, 400)
+
+@app.route("/logout", methods=["DELETE"])
+def logout_user():
+    if request.method == "DELETE":
+        session.clear()
+        return make_response({"message": "Logout successful"}, 200)
+    else:
+        return make_response({"error": f"Invalid request type. (Expected DELETE; received {request.method}.)"}, 400)
 
 ########################################################################
 ########################## Error Handling ##############################
 ########################################################################
 
-# Error handling for 404
 @app.errorhandler(404)
 def not_found(error):
     return make_response(jsonify({"error": "Not found"}), 404)
-
 
 ########################################################################
 ############################# Run the app ##############################
---
+++ auth/authorization_required.py
@@ -0,0 +1,48 @@
+from flask import make_response, session
+from models.model_config import db
+from app import app
+from models import User
+from functools import partial, wraps
+
+#######################################################
+################ AUTHORIZATION LEVELS #################
+#######################################################
+
+BASE_AUTHORIZED_OPERATIONS=["auth_test"] # TODO
+
+#######################################################
+###### EXPORTABLE MIDDLEWARE UTILITY FUNCTION(S) ######
+#######################################################
+
+def authorization_required(func=None):
+
+    # Factory function to tether wrapper to decorator.
+    @wraps(func)
+
+    # Inner authorization function.
+    def decorated_authorizer(*args, **kwargs):
+        # Retrieve existing and matching user ID from server-persistent session storage.
+        # NOTE: Server sessions are NOT THE SAME as database sessions! (`session != db.session`)
+        user_id = session.get("user_id")
+
+        if not user_id:
+            return make_response({"error": "User account not authenticated. Please log in or sign up to continue using the application."}, 401)
+            
+        try:
+            # Query users from database where authorized user exists (has matching ID).
+            authorized_user = User.query.filter(User.id == user_id).first()
+            if authorized_user is None:
+                return make_response({"error": "Invalid username or password. Try again."}, 401)
+        except Exception as error:
+            return make_response({"error": f"Something went wrong.", "details": str(error)}, 500)
+
+        # Invoke wrapped view function with administrative access as output.
+        return func(authorized_user.to_dict(), *args, **kwargs)
+    return decorated_authorizer
+
+"""
+Check level of user
+make variable BASE_AUTHORIZED_OPERATIONS=["auth_test"] and ADMIN_AUTHORIZED_OPERATIONS
+do the checking logic
+func.__name__ == "auth_test"
+"""
